0c78c5dd62ee10ae8f747663a1588a1ab3206246
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-11-05 22:49:47 +0300

    lesson 2_7

--- app.js
+++ app.js
@@ -2,10 +2,16 @@
 const App = {
 	data() {
 		return {
+			inputValue: '',
 			placeholderString: 'Введите название заметки',
 			title: 'Список заметок'
 		};
 	},
+	methods: {
+		inputChangeHandler(evt) {
+			this.inputValue = evt.target.value;
+		}
+	}
 };
 
 Vue.createApp(App).mount('#app');
